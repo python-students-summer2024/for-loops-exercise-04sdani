c88d6decbbe2be5cc140a1eea2838088bbaa2325
04sdani <shriya@danicorp.com>
2024-06-17 01:43:48 -0400

    I have edited all the code and everything is running smoothly and I can not find quite why the code is not passing the tests.

--- number_guessing.py
+++ number_guessing.py
@@ -24,3 +24,20 @@ def guess_number(low, high, num_attempts):
     :param num_attempts: The number of attempts the user is given to guess the correct number.
     :returns: True if the user answers any attempt correctly, False otherwise.
     """
+    import random
+
+    random_integer = random.randint(low,high)
+    
+    print("The range is from " + str(low) + " to " + str(high) + ".")
+    print("You have " + str(num_attempts) + " attempts.")
+
+    for i in range(num_attempts):
+        user_guess = input("Please guess the number: ")
+        if user_guess.isdigit():
+            if user_guess == str((random_integer)):
+                return True
+                break
+            else:
+                continue
+    else:
+        return False
